aacceffdc0de8e2eb5d0a87f23e5bd98e88d3898
Bruno Fontes <brunobfonttes@gmail.com>
2023-06-08 19:45:21 -0300

    initial commit

---
+++ src/main/java/org/example/model/User.java
@@ -0,0 +1,55 @@
+package org.example.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotBlank(message = "The username is required.")
+    private String username;
+
+    @NotBlank(message = "The name is required.")
+    private String name;
+
+    @NotBlank(message = "The email is required.")
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
