38d49e129cefd2c02a825a75c6f30a60478fed10
Bruno Fontes <brunobfonttes@gmail.com>
2023-06-11 21:21:45 -0300

    refactor: replaces getters and setters by decorators

--- src/main/java/org/example/model/User.java
+++ src/main/java/org/example/model/User.java
@@ -4,9 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class User {
     @Id
     @GeneratedValue
@@ -20,36 +23,4 @@ public class User {
 
     @NotBlank(message = "The email is required.")
     private String email;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
